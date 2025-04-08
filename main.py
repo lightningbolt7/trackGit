f55cc86cdcc81ce564c4f7fa358968b0c123a8f0
lightningbolt7 <89177987+lightningbolt7@users.noreply.github.com>
2025-04-08 00:03:39 -0500

    final

--- main.py
+++ main.py
@@ -6,14 +6,31 @@ endpoint = f'https://api.github.com/users/{name}/events'
 response = requests.get(endpoint)
 data = response.json()
 
+tracker = {}
+
 for element in data:
-     action = element["type"]
-     actor = element["actor"]["login"]
-     repo = element["repo"]["name"]
-     date = element["created_at"]
+    action = element["type"]
+    actor = element["actor"]["login"]
+    repo = element["repo"]["name"]
+    date = element["created_at"][0:10]
+
+    key = (action,actor,repo,date)
+    
+    if tracker.get(key) is None:
+        tracker[key] = 1
+    else:
+        tracker[key]+=1
+    
+for element in tracker:
+    result = f"{element[1]} did {tracker.get(element)} {element[0]} on {element[2]} at {element[3]}"
+    print(result)
+        
      
-     result = f"{actor} did {action} on {repo} at {date}"
-     print(result)
+    
+     
+
+
+
      
 
 
